87f6681ead0c118e03f972c7dc02ad5df84de60b
kadabengarann <rio27dh@gmail.com>
2021-07-14 21:26:42 +0800

    update something

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ const mobileNav = () => {
     });
 }
 mobileNav();
+
+document.onkeydown = function(e) {
+    if (e.ctrlKey && 
+        (e.keyCode === 67 || 
+         e.keyCode === 86 || 
+         e.keyCode === 85 || 
+         e.keyCode === 117)) {
+        alert('NOPE');
+        return false;
+    } else {
+        return true;
+    }
+};
