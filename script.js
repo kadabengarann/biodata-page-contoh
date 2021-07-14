fa24872121f8de8eca0e9a116a6361ca7d107dd9
kadabengarann <rio27dh@gmail.com>
2021-07-15 00:06:53 +0800

    update

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const mobileNav=()=>{const burger=document.querySelector('.burger');const nav=document.querySelector('.mbl-nav');burger.addEventListener('click',()=>{nav.classList.toggle('mbl-nav-geser');burger.classList.toggle('toggle')})}
+ const mobileNav=()=>{const burger=document.querySelector('.burger');const nav=document.querySelector('.mbl-nav');burger.addEventListener('click',()=>{nav.classList.toggle('mbl-nav-geser');burger.classList.toggle('toggle')})}
 mobileNav();document.onkeydown=function(e){if(e.ctrlKey&&(e.keyCode===67||e.keyCode===86||e.keyCode===85||e.keyCode===117)){alert('NOPE');return!1}else{return!0}}
